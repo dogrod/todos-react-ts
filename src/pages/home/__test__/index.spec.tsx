3e1043cb1e6c9c264ae221b6e51b082910496616
dogrod <rodrickjue@hotmail.com>
2018-10-09 18:07:47 +0800

    :white_check_mark: Test case for home page

---
+++ src/pages/home/__test__/index.spec.tsx
@@ -0,0 +1,15 @@
+import { shallow } from 'enzyme'
+import * as React from 'react'
+
+import Home from '../index'
+
+describe('Home page', () => {
+  // DOM testing
+  it('renders right content', () => {
+    const component = shallow(
+      <Home />
+    )
+
+    expect(component.text()).toEqual('Awesome Todos APP!')
+  })
+})
