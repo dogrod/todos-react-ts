c3968f420397b4112bb7d45cb27df8d4a9c8f80c
dogrod <rodrickjue@hotmail.com>
2018-10-18 15:27:32 +0800

    :hammer: Use require for enzyme-adapter-react-16 (See reason in annotation)

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,10 @@
 // Set up enzyme adapter
 import * as enzyme from 'enzyme'
-import * as Adapter from 'enzyme-adapter-react-16'
+
+// FIXME: USE IMPORT STATEMENT
+// Enzyme adapter for react 16 has some problem with Typescript when using import.
+// So now use require temporarily.
+// See: https://github.com/airbnb/enzyme/issues/1284
+const Adapter = require('enzyme-adapter-react-16')
 
 enzyme.configure({ adapter: new Adapter() })
